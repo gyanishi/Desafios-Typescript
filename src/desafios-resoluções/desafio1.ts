e3695e9c30936fd8c6663ecdaba22ba0ca104091
Gabriel Yanishi <gabrielyanishi@gmail.com>
2022-06-20 18:32:56 -0300

    Adição da pasta de resolução de desafios e a resolução do desafio 1.

---
+++ src/desafios-resoluções/desafio1.ts
@@ -0,0 +1,10 @@
+//Desafio 1 - Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+interface modeloEmployee {
+    code?: number;
+    name?: string;
+}
+let employee: modeloEmployee = {
+    code: 10,
+    name: "John",
+};
